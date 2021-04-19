ead59bc6ec05fc5ba17fe6b03bebe2779f0e7f0d
Victoria Arguijo <vicky.arguijo@gmail.com>
2021-04-19 00:29:23 -0300

    Navbar and logo

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+import logo from '../../images/logo.png'
+import './navbar.css'
+export const Navbar = () => {
+    return (
+        <header className='header'>
+            <div className='logoContainer'>
+                <img className='logo' src={logo} alt="El Gato Gris Logo" />
+                <p>El Gato Gris</p>
+            </div>
+            <nav>
+                <ul className='nav'>
+                    <li className='navItem'><a href="#">Home</a></li>
+                    <li className='navItem'><a href="#">Productos</a></li>
+                    <li className='navItem'><a href="#">Preguntas Frecuentes</a></li>
+                    <li className='navItem'><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+
+}
