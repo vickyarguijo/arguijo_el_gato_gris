cf4240462a7e2813d6a649e82b7cfa0b4869c8d9
Victoria Arguijo <vicky.arguijo@gmail.com>
2021-05-08 12:04:06 -0300

    pruebas itemlist

--- src/components/itemlist/itemlist.js
+++ src/components/itemlist/itemlist.js
@@ -10,21 +10,24 @@ export const ItemList = (props) => {
     let { categoryId } = useParams()
   
     let filteredProducts = []
-
-  if (categoryId)  {
-	   filteredProducts = products.filter((product) => product.category === categoryId)
-  } else {
-    filteredProducts = products
-  }
+    
+    if (categoryId)  {
+      filteredProducts = products.filter((product) => product.category === categoryId)
+   } else {
+     filteredProducts = products
+   }
   
-   
-
+    
     const getProducts = (products) => { 
         return new Promise ((resolve, reject) => {
             
             setTimeout( () => {
                 resolve(products)}, 2000)
         })
+        .then(
+          setProductData(products)
+          
+        ) 
          .then(
             setProductData(products)
             
@@ -32,25 +35,30 @@ export const ItemList = (props) => {
          
     }
 
+    const showFilteredProducts = (productData) => {
+      return (
+        productData.length >0 ? (productData.map((product) => 
+              <Item id={product.id} 
+                    category={product.category} 
+                    title={product.title} 
+                    price={product.price} 
+                    pictureURL={product.image.pictureURL} />
+         )) : (<p>No tengo productos</p>)
+      )
+  }
+  
     useEffect(
       () => {
-      getProducts(products).then(result => {
-          console.log(result)
-          
-      });
+       
+
       getProducts(filteredProducts)
+
   }, [categoryId])
 
     return (
         <div className="itemList">
            
-           {productData.length >0 ? (productData.map((product) => 
-                <Item id={product.id} 
-                      category={product.category} 
-                      title={product.title} 
-                      price={product.price} 
-                      pictureURL={product.image.pictureURL} />
-           )) : (<p>No tengo productos</p>)}
+           {showFilteredProducts(productData)}
            
         </div>
     )
