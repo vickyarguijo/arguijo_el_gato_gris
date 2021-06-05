8f3d771b0dc5b286875c97e3aff46019991c2cf2
Victoria Arguijo <vicky.arguijo@gmail.com>
2021-06-05 17:15:58 -0300

    estilos confirmation page

--- src/components/confirmation/confirmation.js
+++ src/components/confirmation/confirmation.js
@@ -1,17 +1,23 @@
 import './confirmation.css'
+import cat_in_box from '../../images/cat_in_box.png'
 import {useContext} from 'react'
 import {CartContext} from '../context/cartcontext'
 
 export const Confirmation = () => {
 
-    const {orderId} = useContext(CartContext);
+    const {orderId, cart} = useContext(CartContext);
 
     return (
         <div className="confirmation">
-           
-             <h4>Recibimos tu orden!</h4>
-             <p>Nos contactaremos a la brevedad para acordar el envío y forma de pago.</p>
-             <p>Número de orden de compra: {orderId}</p>
+            <h4 className="confirmation_headline">¡Gracias por tu compra!</h4>
+            <img src={cat_in_box} alt="imagen de gato en una caja" />
+            
+            <p className="confirmation_text">Nos contactaremos a la brevedad para acordar el envío y forma de pago.</p>
+            <p className="confirmation_text"> Tené a mano tu código de compra que aparece a continuación.</p>
+            <p className="confirmation_orderId_text">Tu código de compra: <span className="confirmation_orderId">{orderId}</span></p>
+
+             <p className="confirmation_text">¡Ya falta muy poco para que disfrutes de tu compra!</p>
+            
         </div>
         
     )
