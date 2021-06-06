3e47d1bccfa1a3dc4ae9ae23b87813b9966b85b9
Victoria Arguijo <vicky.arguijo@gmail.com>
2021-06-06 18:35:06 -0300

    validación teléfono

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {Fragment} from 'react'
 
 export const Cart = () => {
-    const {cart, cartTotalPrice,removeItem,clearCart, canBuy, checkIfCanBuy, createOrder} = useContext(CartContext);
+    const {cart, cartTotalPrice,removeItem, clearCart, canBuy, checkIfCanBuy, createOrder} = useContext(CartContext);
    
     const [inputValues, setInputValues] = useState({
         name:'',
@@ -19,16 +19,27 @@ export const Cart = () => {
     const handleChange = ({target: { name, value }}) => setInputValues({ ...inputValues, [name]: value });
   
     const handleBlur = ({target: { name, value }}) => setInputValues({ ...inputValues, [name]: value.trim() }
-
     );
 
-    function onSubmit(e) {
-        e.preventDefault();
-    
-        if([name, surname, phone, email].includes('')) {
+    const handleTel = (e) => {
+        if(!((e.key == 1) || (e.key == 2) || (e.key == 3) || (e.key == 4) || (e.key == 5) || (e.key == 6) || (e.key == 7) || (e.key == 8) || (e.key == 9) || (e.key == 0) || (e.key == "ArrowLeft") || (e.key == "ArrowRight") || (e.key == "Backspace")) ) {
+           e.preventDefault();
+        }
+    }
+
+    function checkFormValues(e) {
+        
+         if([name, surname, phone, email].includes('')) {
           setError(true);
           return;
         }
+        if(phone.length < 8){
+            setError(true);
+          return;
+        }
+        if(error){
+            e.preventDefault();
+        } 
     }
 
     useEffect( () => {
@@ -62,31 +73,31 @@ export const Cart = () => {
                     <p className='cartTotal'>Total de tu compra: ${cartTotalPrice}</p>
                     
                     <h3 className='cart_h3'>Completá tus datos para finalizar la compra:</h3>
-                    <form onSubmit={onSubmit}>
+                    <form>
                         <div className="form_data_container">
                             <div className='form_container1'>
                                 <label>Nombre:</label>
+                                <input type="text" name="name" value={name} onChange={handleChange} onBlur={handleBlur, checkFormValues} placeholder="Nombre" />
                                 {error && <span className='errorMessage'>*Debes completar este campo para finalizar la orden de compra. </span>}
-                                <input type="text" name="name" value={name} onChange={handleChange} onBlur={handleBlur} placeholder="Nombre" />
 
                                 <label>Apellido:</label>
+                                <input type="text" name="surname" value={surname} onChange={handleChange} onBlur={handleBlur, checkFormValues} placeholder="Apellido" />
                                 {error && <span className='errorMessage'>*Debes completar este campo para finalizar la orden de compra. </span>}
-                                <input type="text" name="surname" value={surname} onChange={handleChange} onBlur={handleBlur} placeholder="Apellido" />
                             </div>
                         
                             <div className='form_container2'>
                                 <label>Teléfono:</label>
-                                {error && <span className='errorMessage'>*Debes completar este campo para finalizar la orden de compra. </span>}
-                                <input type="tel" name="phone" value={phone} onChange={handleChange} onBlur={handleBlur} placeholder="(+5411)555-5555" />
+                                <input type="tel" name="phone" value={phone} onKeyDown={handleTel} onChange={handleChange} onBlur={handleBlur, checkFormValues} placeholder="(+5411)555-5555" />
+                                {error && <span className='errorMessage'>*Tu teléfono debe tener un mínimo de 8 dígitos, sin guiones, paréntesis ni espacios. </span>}
 
                                 <label>E-mail:</label>
+                                <input type="email" name="email" value={email} onChange={handleChange} onBlur={handleBlur, checkFormValues} placeholder="example@example.com" />
                                 {error && <span className='errorMessage'>*Debes completar este campo para finalizar la orden de compra. </span>}
-                                <input type="email" name="email" value={email} onChange={handleChange} onBlur={handleBlur} placeholder="example@example.com" />
                             </div>
                         </div>
                         <div className='form_button_container'>
                             <Link to={canBuy ? '/confirmation' : '/cart'}>
-                                <button className='button button_primary' onClick={()=> createOrder(inputValues)} type="submit" disabled={[name, surname, phone, email].includes('')}>Comprar</button>
+                                <button className='button button_primary' onClick={()=> createOrder(inputValues)} type="submit" disabled={[name, surname, phone, email].includes('') || phone.length < 8}>Comprar</button>
                             </Link>
                         </div>
                     </form>
